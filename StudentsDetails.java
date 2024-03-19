20d721534b44f364f8878f9afac019ef5cb0c239
joymunene <163843844+joymunene@users.noreply.github.com>
2024-03-19 19:45:51 +0300

    Add files via upload

---
+++ StudentsDetails.java
@@ -0,0 +1,22 @@
+import java.uti.Scanner;
+public class studentsDetails{
+public static void main(string[] args){
+Scanner =new Scanner(System.In);
+System.out.printIn("Enter your marks")
+int marks= scanner nextInt();
+if (marks<=49){
+system.out.printIn("F");
+else if (marks>=50 $$marks<59){
+System.out.printIn("C");
+}
+else if (marks>=60 $$marks<69){
+System.out.printIn("B");
+}
+else if (marks>=70 $$marks<100){
+System.out.printIn("A");
+}
+else if (marks>=100){
+System.out.printIn("NO MARKS AWARDED");
+}
+}
+
